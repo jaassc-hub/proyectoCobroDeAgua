8d19bad59532da6ad5b93dfe34a61ce65ad26e8d
junta <juntaadmondeagua.santacruz@gmail.com>
2026-02-10 14:33:19 -0600

    Forzando el app de wsgi

--- managejaasc/settings.py
+++ managejaasc/settings.py
@@ -85,12 +85,12 @@ WSGI_APPLICATION = 'managejaasc.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
-
 db_from_env = dj_database_url.config(conn_max_age=600)
 if db_from_env:
     DATABASES['default'] = db_from_env
