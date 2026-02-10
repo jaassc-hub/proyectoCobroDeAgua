aaf16ac8e498078cdb6f2195856b4d4558329162
junta <juntaadmondeagua.santacruz@gmail.com>
2026-02-10 14:39:09 -0600

    Agregando migraciones y superususario

--- managejaasc/wsgi.py
+++ managejaasc/wsgi.py
@@ -14,4 +14,15 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'managejaasc.settings')
 
 application = get_wsgi_application()
 
+try:
+    print("Iniciando migraciones...")
+    call_command('migrate', interactive=False)
+except Exception as e:
+    print(f"Error en migraciones: {e}")
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'junta', '010325')
+
 app = application
