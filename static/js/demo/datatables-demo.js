c7cef36e7bb54e8ba036d35336fe4c323cb1affa
junta <juntaadmondeagua.santacruz@gmail.com>
2026-02-11 21:45:08 -0600

    Eliminacion de archivos staticos y uso de CDN

--- static/js/demo/datatables-demo.js
+++
@@ -1,9 +0,0 @@
-// Call the dataTables jQuery plugin
-$(document).ready(function() {
-  $('#dataTable').DataTable({
-    // FUERZA A USAR EL RENDERIZADO DE BOOTSTRAP PARA LA PÁGINACIÓN Y EL CONTENEDOR
-    "renderer": "bootstrap", 
-    // Asegura que los números de paginación se muestren correctamente
-    "pagingType": "full_numbers" 
-  });
-});
